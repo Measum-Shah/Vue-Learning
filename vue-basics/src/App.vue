61d96bbbe23e2469a4b5d4ea37ff17e0398ec239
Measum-Shah <shahmeasum008@gmail.com>
2025-08-01 12:10:03 +0500

    learned text interpolation

---
+++ vue-basics/src/App.vue
@@ -0,0 +1,13 @@
+<template lang="html">
+  <div>
+    <!-- This component displays text interpolation using mustache syntax -->
+    <TextInterpolation />
+   
+  </div>
+</template>
+<script>
+import TextInterpolation from './components/TextInterpolation.vue';
+</script>
+<style lang="">
+  
+</style>
