c137d0b00da3ec81f7d8955efb8e4883293a7e75
Measum-Shah <shahmeasum008@gmail.com>
2025-08-01 15:59:16 +0500

    Learned Directives

--- vue-basics/src/App.vue
+++ vue-basics/src/App.vue
@@ -1,15 +1,21 @@
 <template lang="html">
   <div>
+    <!-- You can learn any concept by un commenting that component in the App.vue file. -->
+
     <!-- This component displays text interpolation using mustache syntax -->
-    <TextInterpolation />
-      <hr>
+    <!-- <TextInterpolation /> -->
+
     <!-- This Component will show attribute binding -->
-   <AttributeBinding/>
+   <!-- <AttributeBinding/> -->
+
+   <!-- Thi Component shows basic use of directives -->
+    <Directives />
   </div>
 </template>
 <script setup lang="js">
 import AttributeBinding from './components/AttributeBinding.vue';
 import TextInterpolation from './components/TextInterpolation.vue';
+import Directives from './components/Directives.vue';
 </script>
 <style lang="">
   
---
+++ vue-basics/src/components/Directives.vue
@@ -0,0 +1,41 @@
+<template lang="html">
+    <div>
+    <h1>Directives</h1>
+    <h1 class="week">Day Checker</h1>
+    <!-- dont get worried about v-mondal think it as data is transferred as wriiten in inpurt to the variable in real time -->
+    <input type="text" v-model="day" placeholder="Enter a number of day of the week" />
+    <p v-if="day == 1">Monday</p>
+    <p v-else-if="day == 2">Tuesday</p>
+    <p v-else-if="day == 3">Wednesday</p>
+    <p v-else-if="day == 4">Thursday</p>
+    <p v-else-if="day == 5">Friday</p>
+    <p v-else-if="day == 6">Saturday</p>        
+    <p v-else-if="day == 7">Sunday</p>
+    <p v-else>Invalid day number</p>
+    </div>
+
+    <!-- so we see directives as v-if v-else-if v-else -->
+     <!-- There are more such as v-on:click -->
+      <hr>
+      <!-- color changer -->
+    <h1 class="title">Color Changer</h1>
+   <p>select the color</p>
+   <input type="text" v-model="color" placeholder="Enter a color"/>
+   <button v-on:click="changecolor">Change color</button>
+</template>
+<script setup lang="js">
+import { ref } from 'vue';
+// Dont focus on ref much at this point, we will cover it in detail later
+// ref is a function that creates a reactive reference to a value
+// It is used to create reactive variables in Vue.js
+// just know its just a variable
+const day = ref('');
+const color = ref('');
+const changecolor = () => {
+    window.document.querySelector('.title').style.color =  color.value;
+}
+
+</script>
+<style lang="">
+  
+</style>
