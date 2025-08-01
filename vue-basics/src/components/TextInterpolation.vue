d8e78e02a6583f3831ea9ff763ea0851752a3dcd
Measum-Shah <shahmeasum008@gmail.com>
2025-08-01 12:10:27 +0500

    learned text interpolation

---
+++ vue-basics/src/components/TextInterpolation.vue
@@ -0,0 +1,22 @@
+<template lang="html">
+  <div>
+    <h1>{{ name }}</h1> 
+<!-- this syntax is called as mustache syntax, it is used to bind data to the template also known as text interpolation    -->
+    <p>{{ message }}</p>
+  </div>
+</template>
+<script setup  lang="js">
+
+
+
+// these variables are bind above to the template using mustache syntax
+  const name = "vue Basics";
+  const message = "Welcome to Vue.js Basics!";
+  
+
+
+
+</script>
+<style lang="">
+  
+</style>
